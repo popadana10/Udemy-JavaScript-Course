c85a08c5381b246b1fda48a74d3495e777a4251e
Popa Dana-Mihaela <danapopa@Popas-MacBook-Pro.local>
2023-10-08 14:31:44 +0300

    first commit

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+document.querySelector("#open-nav-menu").addEventListener("click", function(){});
 
-console.log(4+5);
+document.querySelector("#close-nav-menu").addEventListener("click", function(){});
+
+const customer = "Sara";
+let balance = 2000;
+
+console.log("Hi,"+ customer + ".Your balance is USD" + balance );
+
+balance = balance + 200;
+
+console.log("Hi,"+ customer + ".Your new balance is USD" + balance );
+
